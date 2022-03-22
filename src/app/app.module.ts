d14ad0559a584d1b4bcb8508ea1042f86aee19fd
Adrián Rivera <rivera.correa88@hotmail.com>
2022-03-22 17:36:34 -0500

    added authentication with Azure B2C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,63 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WebapiComponent } from './webapi/webapi.component';
+
+import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+import { msalConfig, protectedResources } from './auth-config';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Add the essential Angular materials.
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    WebapiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    HttpClientModule,
+    MsalModule.forRoot(new PublicClientApplication(msalConfig),
+      {
+        // The routing guard configuration.
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: protectedResources.todoListApi.scopes
+        }
+      },
+      {
+        // MSAL interceptor configuration.
+        // The protected resource mapping maps your web API with the corresponding app scopes. If your code needs to call another web API, add the URI mapping here.
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([
+          [protectedResources.todoListApi.endpoint, protectedResources.todoListApi.scopes]
+        ])
+      })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalGuard
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
---
+++ src/app/auth-config.ts
@@ -0,0 +1,48 @@
+import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+export const b2cPolicies = {
+     names: { //Azure user flow or custom policies
+         signUpSignIn: "B2C_1_signupsignin1",
+     },
+     authorities: {
+         signUpSignIn: {
+             authority: "https://arcb2c.b2clogin.com/arcb2c.onmicrosoft.com/B2C_1_signupsignin1",
+         },
+     },
+     authorityDomain: "arcb2c.b2clogin.com"
+ };
+
+
+export const msalConfig: Configuration = {
+     auth: {
+         clientId: 'f696247b-13e1-4499-b741-fd7f1e92c290',
+         authority: b2cPolicies.authorities.signUpSignIn.authority,
+         knownAuthorities: [b2cPolicies.authorityDomain],
+         redirectUri: '/',
+     },
+     cache: {
+         cacheLocation: BrowserCacheLocation.LocalStorage,
+         storeAuthStateInCookie: isIE,
+     },
+     system: {
+         loggerOptions: {
+            loggerCallback: (logLevel, message, containsPii) => {
+                console.log(message);
+             },
+             logLevel: LogLevel.Verbose,
+             piiLoggingEnabled: false
+         }
+     }
+ }
+
+export const protectedResources = { //API paths and scopes
+  todoListApi: {
+    endpoint: "http://localhost:5000/api/todolist",
+    scopes: ["https://arcb2c.onmicrosoft.com/api/tasks.read"],
+  },
+}
+export const loginRequest = {
+  scopes: []
+};
